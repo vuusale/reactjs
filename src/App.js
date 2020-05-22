a7156aaa31bbe0c58759c735cfd029880f704c70
Vusala <vuusale@gmail.com>
2020-05-22 17:18:24 +0400

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react'; 
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
 import './App.css';
 
 class App extends Component { //different
   render() { //different
     // The rest of the file is the same
     return (
-      <div className="App"> 
+      <div> 
         <Navbar dark color="primary">
           <div className="container">
             <NavbarBrand href="/">Restaurant</NavbarBrand>
           </div>
         </Navbar>
+        <Menu />
       </div>
     )
   };

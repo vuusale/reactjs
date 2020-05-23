86eb6e438a412726f00721955e6745176b7549c8
Vusala <vuusale@gmail.com>
2020-05-23 23:49:08 +0400

    Restaurant name

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -23,7 +23,7 @@ class Main extends Component {
       <div> 
         <Navbar dark color="primary">
           <div className="container">
-            <NavbarBrand href="/">Restaurant</NavbarBrand>
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
         <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />

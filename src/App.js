8e504d1cb4e8957d34bf19c1ee03327ffe09aae8
Vusala <vuusale@gmail.com>
2020-05-23 23:47:33 +0400

    Dishdetail and presentational, container components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
 import React, { Component } from 'react'; 
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponent';
+import Main from './components/MainComponent';
 import './App.css';
-import { DISHES } from './shared/dishes';
 
 class App extends Component {
 
-  constructor() {
-    super();
-    this.state = {
-      dishes: DISHES
-    };
-  }
-
   render() {
     return (
-      <div> 
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Restaurant</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes} />
+      <div>
+        <Main />
       </div>
     )
   };

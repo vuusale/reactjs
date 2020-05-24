000c18af5f56ee07f752042ad4307a7fdfd96f4c
Vusala <vuusale@gmail.com>
2020-05-24 23:05:14 +0400

    Header and footer

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'; 
-import { Navbar, NavbarBrand } from 'reactstrap';
 // import Menu from './MenuComponent';
 // import Dishdetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
